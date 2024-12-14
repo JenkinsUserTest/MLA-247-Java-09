0667b572daca735a8341ffacee4cf7152e5b53ce
JenkinsUserTest <73050663+JenkinsUserTest@users.noreply.github.com>
2024-12-14 11:01:22 +0530

    Add files via upload

---
+++ GroupBy.sql
@@ -0,0 +1,58 @@
+use hr_schema;
+
+select max(salary) from employees;
+
+select avg(salary),max(salary),min(salary),sum(salary)
+from employees
+where job_id like '%REP%';
+
+select min(hire_date),max(hire_Date)
+from employees;
+
+select count(*) 
+from employees;
+
+select count(*)
+from employees
+where department_id=10;
+
+
+select avg(ifnull(commission_pct,0))
+from employees;
+
+
+select department_id,avg(salary)
+from employees
+group by department_id;
+
+
+select department_id,job_id,sum(salary)
+from employees
+group by department_id,job_id;
+
+
+
+select department_id,job_id,sum(salary)
+from employees
+where department_id >40
+group by department_id,job_id
+order by department_id;
+
+
+select department_id,job_id,sum(salary)
+from employees
+group by department_id,job_id with rollup;
+
+
+select department_id,max(salary)
+from employees
+group by department_id
+having max(salary)>10000;
+
+
+/*select max(avg(salary))
+from employees;*/
+
+
+select employee_id,department_id
+from employees;
---
+++ Joins.sql
@@ -0,0 +1,80 @@
+/* natural join */
+use hr_schema;
+select employee_id,first_name,last_name,department_id,department_name
+from employees
+natural join
+departments;
+
+select employee_id,last_name,location_id,department_id
+from employees join departments
+using(department_id);
+
+select employee_id,last_name,e.department_id,department_name
+from employees e
+inner join departments d
+on e.department_id=d.department_id;
+
+
+select employee_id,city,department_name
+from employees e
+join departments d
+on e.department_id=d.department_id
+join locations l
+on d.location_id=l.location_id;
+
+
+select employee_id,department_name
+from employees e
+join departments d
+on (e.department_id=d.department_id)
+and e.manager_id=149;
+
+select e.last_name,e.department_id,d.department_name
+from employees e
+left outer join
+departments d
+on(e.department_id=d.department_id);
+
+
+select e.last_name,d.department_id,d.department_name
+from employees e
+right outer join
+departments d
+on(e.department_id=d.department_id);
+
+
+
+select e.last_name,e.department_id,d.department_name
+from employees e
+left outer join
+departments d
+on(e.department_id=d.department_id)
+union
+select e.last_name,d.department_id,d.department_name
+from employees e
+right outer join
+departments d
+on(e.department_id=d.department_id);
+
+
+select worker.last_name as "Worker'sLastName",manager.last_name as "Manager'sLastName"
+from employees worker
+join
+employees manager
+on(worker.manager_id=manager.employee_id);
+
+
+select last_name,department_name
+from employees
+cross join departments;
+
+
+select e.last_name,e.hire_date,j.job_id
+from employees e
+join job_history j
+on e.hire_date
+between j.start_date and j.end_date;
+
+
+select last_name,salary from employees where last_name='De Haan';
+select * from employees where salary>17000;

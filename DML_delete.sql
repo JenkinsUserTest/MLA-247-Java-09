a6d59ef0fb86bf54110b119af061341f19c59d33
JenkinsUserTest <73050663+JenkinsUserTest@users.noreply.github.com>
2024-12-14 15:12:40 +0530

    Add files via upload

---
+++ DML_delete.sql
@@ -0,0 +1,20 @@
+use hr;
+delete from employees
+where employee_id=1001;
+
+select * from employees where employee_id=1001;
+
+select * from employees;
+
+delete from employees
+where employee_id>201
+order by employee_id desc
+limit 5;
+
+delete  from departments
+where department_id=10;
+
+use test;
+
+delete from courses
+where course_id='C001';
